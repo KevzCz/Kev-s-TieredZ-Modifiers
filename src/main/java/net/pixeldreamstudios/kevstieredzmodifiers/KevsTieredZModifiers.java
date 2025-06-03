85237cc3b05c864ae020bdd682157672acff384b
KevinCz <lenguyenhoangthanh123@gmail.com>
2025-06-03 12:35:44 +0700

    added druids compat

--- src/main/java/net/pixeldreamstudios/kevstieredzmodifiers/KevsTieredZModifiers.java
+++ src/main/java/net/pixeldreamstudios/kevstieredzmodifiers/KevsTieredZModifiers.java
@@ -58,6 +58,16 @@ public class KevsTieredZModifiers implements ModInitializer {
 				);
 			});
 		}
+		// DRUIDS COMPAT
+		if (FabricLoader.getInstance().isModLoaded("druids")) {
+			FabricLoader.getInstance().getModContainer(MOD_ID).ifPresent(modContainer -> {
+				ResourceManagerHelper.registerBuiltinResourcePack(
+						Identifier.of(MOD_ID, "druids_compat"),
+						modContainer,
+						ResourcePackActivationType.ALWAYS_ENABLED
+				);
+			});
+		}
 		// MYTHIC METALS COMPAT
 		if (FabricLoader.getInstance().isModLoaded("mythicmetals")) {
 			FabricLoader.getInstance().getModContainer(MOD_ID).ifPresent(modContainer -> {
